edd070d6240859374cd8aae4aae7fb646de43258
othmanBouranja <bouranjaothmane@gmail.com>
2024-05-21 11:58:17 +0100

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,104 +108,17 @@
 	<!-- Logo Nav START -->
 	<nav class="navbar navbar-expand-xl">
 		<div class="container">
+
 			<!-- Logo START -->
-			<a class="navbar-brand me-0" href="/">
-				<img class="light-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="logo">
-				<img class="dark-mode-item navbar-brand-item" src="assets/images/logo-light.svg" alt="logo">
-			</a>
+				<x-inc.header.logo />
 			<!-- Logo END -->
 
 			<!-- Main navbar START -->
-			<div class="navbar-collapse collapse" id="navbarCollapse">
-				<ul class="navbar-nav navbar-nav-scroll dropdown-hover mx-auto">
-
-					<li class="nav-item">
-						<a class="nav-link{{ Request::is('/') ? ' active' : '' }}" href="/">الرئيسية</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link{{ Request::is('about') ? ' active' : '' }}" href="/about">من نحن</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link{{ Request::is('services') ? ' active' : '' }}" href="/services">خدماتنا</a>
-						
-					</li>
-					{{-- <li class="nav-item">
-						<a class="nav-link{{ Request::is('ourteam') ? ' active' : '' }}" href="/ourteam">فريقنا</a>
-					</li> --}}
-					
-					<li class="nav-item">
-						<a class="nav-link{{ Request::is('projects') ? ' active' : '' }}" href="/projects">أعمالنا</a>
-					</li>
-                 
-
-
-					<!-- Nav item -->
-					{{-- <li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-haspopup="true" aria-expanded="false">Pages</a>
-						<ul class="dropdown-menu">
-							<!-- Dropdown submenu -->				
-							<li> <a class="dropdown-item" href="/about">About</a></li>
-							<li> <a class="dropdown-item" href="/pricing">Pricing</a></li>
-							<li> <a class="dropdown-item" href="/faq">FAQ</a></li>
-							<li> <a class="dropdown-item" href="/conditions">Terms & Conditions</a></li>
-		
-						</ul>
-					</li> --}}
-
-					
-
-					
-					<!-- Nav item -->
-					<li class="nav-item"> <a class="nav-link{{ Request::is('contact') ? ' active' : '' }}"" href="/contact">تواصل معنا </a> </li>
-				</ul>
-			</div>
+				<x-inc.header.menu />
 			<!-- Main navbar END -->
 
 			<!-- Buttons -->
 			<ul class="nav align-items-center dropdown-hover ms-sm-2">
-				<!-- Dark mode option START -->
-				{{-- <li class="nav-item dropdown dropdown-animation">
-					<button class="btn btn-link mb-0 px-2 lh-1" id="bd-theme"
-					type="button"
-					aria-expanded="false"
-					data-bs-toggle="dropdown"
-					data-bs-display="static">
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  class="bi bi-circle-half theme-icon-active fill-mode fa-fw" viewBox="0 0 16 16">
-						<path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-						<use href="#"></use>
-					</svg>
-					</button>
-
-					<ul class="dropdown-menu min-w-auto dropdown-menu-end" aria-labelledby="bd-theme">
-						<li class="mb-1">
-							<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light">
-								<svg width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill fa-fw mode-switch me-1" viewBox="0 0 16 16">
-									<path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-									<use href="#"></use>
-								</svg>Light
-							</button>
-						</li>
-						<li class="mb-1">
-							<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill fa-fw mode-switch me-1" viewBox="0 0 16 16">
-									<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
-									<path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
-									<use href="#"></use>
-								</svg>Dark
-							</button>
-						</li>
-						<li>
-							<button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle-half fa-fw mode-switch me-1" viewBox="0 0 16 16">
-									<path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-									<use href="#"></use>
-								</svg>Auto
-							</button>
-						</li>
-					</ul>
-				</li> --}}
-				<!-- Dark mode option END -->
-
 				<!-- Sign up button -->
 				<li class="nav-item me-2 d-none d-sm-block">
 					<a href="/contact" style="background-color: #07b850 ; color:#ffffff" class="btn btn-sm btn-light mb-0">اتخذ خطوة</a>
@@ -263,12 +176,12 @@ Footer START -->
 			<!-- Widget 1 START -->
 			<div class="col-lg-3">
 				<!-- logo -->
-				<a class="me-0" href="index.html">
-					<img class="light-mode-item h-40px" src="assets/images/logo.svg" alt="logo">
-					<img class="dark-mode-item h-40px" src="assets/images/logo-light.svg" alt="logo">
-				</a>
+					<!-- Logo START -->
+					<x-inc.header.logo />
+					<!-- Logo END -->
+					
+					<x-inc.info.description-site />
 
-				<p class="mt-4 mb-2"> لوجيسيل لاب هي شريكك الموثوق لتحقيق نجاحك في العالم الرقمي، مرحبا بك لدى شريكك المثالي لتكبير وإنجاح مشروعك.				</p>
 			</div>
 			<!-- Widget 1 END -->
 
@@ -276,28 +189,11 @@ Footer START -->
 			<div class="col-lg-8 col-xxl-7">
 				<div class="row g-4">
 					<!-- Link block -->
-					<div class="col-6 col-md-4">
-						<h6 class="mb-2 mb-md-4">روابط سريعة</h6>
-						<ul class="nav flex-column">
-							<a class="nav-link" href="/about">من نحن</a>
-							{{-- <a class="nav-link" href="/ourteam">فريقنا</a> --}}
-							<a class="nav-link" href="/projects">أعمالنا</a>
-							<a class="nav-link" href="/services">خدماتنا</a>
-							<a class="nav-link" href="/contact">تواصل معنا</a>
-						</ul>
-					</div>
+					<x-inc.footer.quick-links />
 
-					<div class="col-6 col-md-4">
-						<h6 class="mb-2 mb-md-4">الخدمات</h6>
-						<ul class="nav flex-column">
-							<a class="nav-link" href="/services">تطبيقات الويب</a>
-							<a class="nav-link" href="/services">تطبيقات الجوال</a>
-							<a class="nav-link" href="/services">تطبيقات سطح المكتب</a>
-							<a class="nav-link" href="/services">العلامة التجارية</a>
-							<a class="nav-link" href="/services">خدمات الاستضافة</a>
-						</ul>
-					</div>
-
+				
+					<!-- service block -->
+					<x-inc.footer.quick-service />
 			
 					<!-- Link block -->
 					<div class="col-md-4">
@@ -316,40 +212,18 @@ Footer START -->
 						<!-- Social buttons -->
 						<h6 class="mb-2 mb-md-4">تابعنا على</h6>
 
-						<ul class="list-inline mb-0 mt-3">
-							<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-facebook-f lh-base"></i></a> </li>
-							<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-instagram lh-base"></i></a> </li>
-							<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-twitter lh-base"></i></a> </li>
-							<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-linkedin-in lh-base"></i></a> </li>
-							<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-youtube lh-base"></i></a> </li>
-						</ul>
+							<x-inc.footer.icons />
+						<!-- Social buttons -->
+
 					</div>
 				</div>
 			</div>
 			<!-- Widget 2 END -->
 		</div>
 
-		<!-- Divider -->
-		<hr class="mt-4 mb-0">
 
 		<!-- Bottom footer -->
-		<div class="d-md-flex justify-content-between align-items-center text-center text-lg-start py-4">
-			<!-- copyright text -->
-			<div class="text-body m-auto"><p class="text-white font-weight-bold"> © 2024 جميع الحقوق محفوظة لشركة لوجيسيل لاب</p>
-			</div>
-			<!-- copyright links-->
-			<!-- Language selector -->
-			{{-- <div class="dropdown dropup text-center text-md-end mt-3 mt-md-0">
-				<a class="dropdown-toggle btn btn-sm btn-light mb-0" href="#" role="button" id="languageSwitcher" data-bs-toggle="dropdown" aria-expanded="false">
-					<i class="fas fa-globe me-2"></i>Language
-				</a>
-				<ul class="dropdown-menu min-w-auto" aria-labelledby="languageSwitcher">
-					<li><a class="dropdown-item me-4" href="#"><img class="fa-fw me-2" src="assets/images/flags/uk.svg" alt="">English</a></li>
-					<li><a class="dropdown-item me-4" href="#"><img class="fa-fw me-2" src="assets/images/flags/gr.svg" alt="">German </a></li>
-					<li><a class="dropdown-item me-4" href="#"><img class="fa-fw me-2" src="assets/images/flags/sp.svg" alt="">French</a></li>
-				</ul>
-			</div> --}}
-		</div>
+		<x-inc.footer.copyright />
 	</div>
 </footer>
 <!-- =======================
