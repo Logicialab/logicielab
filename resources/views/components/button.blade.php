4506276bea3152c445d3c67516bfa12e709c4f42
Othman bouranja <76685815+OthmanBouranja@users.noreply.github.com>
2024-10-24 17:45:28 +0100

    button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,6 @@
 <button type="button"
-    class="base-btn text-xl rounded-lg text-[#28398c] hover:text-white bg-white hover:bg-[#28398c] p-[2px] mt-8 bg-gradient-to-r from-[#3336cc] to-[#f6361d]">
-    <div class="bg-white rounded-lg p-2.5 px-8">
-        <a href="{{ $url }}">{{ $name }}</a> <i class="fa-solid fa-arrow-left mr-1"></i>
+    class="base-btn text-xl rounded-lg text-[#28398c] hover:bg-gradient-to-r hover:from-[#3336cc] hover:to-[#f6361d] hover:text-white p-[2px] mt-8 bg-gradient-to-r from-[#3336cc] to-[#f6361d]">
+    <div class="bg-white rounded-lg p-2.5 px-8 hover:bg-transparent">
+        <a href="{{ $url }}" class="hover:text-white">{{ $name }}</a> <i class="fa-solid fa-arrow-left mr-1 hover:text-white"></i>
     </div>
 </button>
